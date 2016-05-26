06f0aa3f8afcc3ba7a51045466ec38fed2dead47
Leonard Xu <leopard.lie@gmail.com>
2016-05-26 19:15:02 +0800

    real search

--- webserver/static/js/app.js
+++ webserver/static/js/app.js
@@ -1,26 +1,7 @@
 var searchApp = angular.module('searchApp', []);
 
 searchApp.controller('searchController', function searchController($scope, $http, $location) {
-//      $scope.results = [
-//    {
-//      title: 'Nexus S',
-//      snippet: 'Fast just got faster with Nexus S.'
-//    }, {
-//      title: 'Motorola XOOM™ with Wi-Fi',
-//      snippet: 'The Next, Next Generation tablet.'
-//    }, {
-//      title: 'MOTOROLA XOOM™',
-//      snippet: 'The Next, Next Generation tablet.'
-//    }
-//  ];
-//    var tmp = $location.search();
-//    console.log(tmp);
-    $http({
-        url: "/api/search",
-        method: "GET",
-        params: {"search":"abc"}//$location.search()
-    }).success(function(data) {
-        console.log(data);
+    $http.get("/api/search" + location.search).success(function(data) {
         data.forEach(function(entry) {
             if (entry.title.length > 20) {
                 entry.title = entry.title.substr(0, 20) + "...";
