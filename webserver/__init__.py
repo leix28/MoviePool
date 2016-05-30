56ae4ebc91c881fea6d2264cb0e4a237ce7c3a15
zhang <zz593141477@gmail.com>
2016-05-30 22:35:11 +0800

    organize code

--- webserver/__init__.py
+++ webserver/__init__.py
@@ -1,34 +1,5 @@
-from flask import Flask, render_template, request
-import db
-import logging
-import json
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    search = request.args.get('search')
-    if search is None:
-        return app.send_static_file('html/index.html')
-    else:
-        results = db.search(search, start=0, count=20)
-        results = map(db.getDoubanBasic, results)
-        return app.send_static_file('html/list.html')
-
-@app.route('/movie/<id>')
-def movie(id):
-    return app.send_static_file('html/movie.html')
-
-@app.route('/api/search')
-def search_api():
-    search = request.args.get('search')
-    results = db.search(search, start=0, count=20)
-    results = map(db.getDoubanBasic, results)
-    results = filter(lambda x: not x is None, results)
-    for item in results:
-        del item['_id']
-    return json.dumps(results)
-
-@app.route('/api/movie/<id>')
-def movie_api(id):
-    return id
+from . import route #make decorators work
---
+++ webserver/route.py
@@ -0,0 +1,34 @@
+from flask import render_template, request
+import db
+import logging
+import json
+
+from . import app
+
+@app.route('/')
+def index():
+    search = request.args.get('search')
+    if search is None:
+        return app.send_static_file('html/index.html')
+    else:
+        results = db.search(search, start=0, count=20)
+        results = map(db.getDoubanBasic, results)
+        return app.send_static_file('html/list.html')
+
+@app.route('/movie/<id>')
+def movie(id):
+    return app.send_static_file('html/movie.html')
+
+@app.route('/api/search')
+def search_api():
+    search = request.args.get('search')
+    results = db.search(search, start=0, count=20)
+    results = map(db.getDoubanBasic, results)
+    results = filter(lambda x: not x is None, results)
+    for item in results:
+        del item['_id']
+    return json.dumps(results)
+
+@app.route('/api/movie/<id>')
+def movie_api(id):
+    return id
